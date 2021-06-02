4f144585b916ef2bbed16f61320f7b8a734b27ee
manisha54 <manishachaudhary2057@gmail.com>
2021-06-02 15:08:15 +0545

    sum commit

---
+++ LabOne/Third.py
@@ -0,0 +1,12 @@
+'''
+3.N students take K apples and distribute them among each other evenly.
+ The remaining (the undivisible) part remains in the basket.
+ How many apples will each single student get? How many apples will remain in the basket?
+ The program reads the numbers N and K. It should print the two answers for the questions above.
+'''
+N=int(input("enter the number of students: "))
+K=int(input("enter the number of apples: "))
+distribute=K/N
+remaning=K%N
+print(f"The number of apples each student gets is {distribute}")
+print(f"The remaning apples in the basket is {remaning}")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 a=10
-b=5
-print(a+True)
-print(b+False)
-a=(1,2,3,4)
-d={1:'Manisha','add
+print(type(a))
+name="manisha"
+print(name)
+
+print(type(name))
