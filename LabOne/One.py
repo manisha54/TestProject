75263fcc3d5eeff3f4236a85f6af15bfafcbf1cf
manisha54 <manishachaudhary2057@gmail.com>
2021-06-02 10:10:32 +0545

    first commit

--- LabOne/One.py
+++ LabOne/One.py
@@ -6,4 +6,4 @@ num1 = int(input("enter the first value: "))
 num2 = int(input("enter the second value: "))
 num3 = int(input("enter the third value: "))
 sum = num1 + num2 + num3
-print(f"the sum of given three numbers is {sum}")
+print(f"the sum  of given three numbers is {sum}")
---
+++ LabOne/Two.py
@@ -0,0 +1,8 @@
+'''
+2.Write a program that reads the length of the base and the height of a right-angled triangle and prints the area.
+ Every number is given on a separate line.
+'''
+l=10
+h=20
+area =l*h/2
+print("The area of the right angled triangle is: ",area)
